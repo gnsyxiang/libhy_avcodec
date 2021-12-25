bd09352692ba6ee327dc109475778add9b686de4
zhenquan.qiu <gnsyxiang@163.com>
2021-12-25 16:49:58 +0800

    build: update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 help_info()
 {
-    echo "eg: ./build.sh pc/arm/mcu [_build]"
+    echo "eg: ./build.sh pc/arm [_build]"
     exit
 }
 
@@ -29,6 +29,7 @@ _cppflags_com="${_cppflags_com} -pipe"
 _cppflags_com="${_cppflags_com} -ffunction-sections"
 _cppflags_com="${_cppflags_com} -fdata-sections"
 _cppflags_com="${_cppflags_com} -fstack-protector-all"
+
 _ldflag_com="${_ldflag_com} -rdynamic"
 _ldflag_com="${_ldflag_com} -Wl,--gc-sections"
 _ldflag_com="${_ldflag_com} -Wl,--as-needed"
@@ -42,43 +43,33 @@ elif [ x$1 = x"arm" ]; then
     gcc_version=arm-himix200-linux
     gcc_prefix=arm-himix200-linux
     cross_gcc_path=${data_disk_path}/opt/toolchains/${vender}/${gcc_version}/bin/${gcc_prefix}-
-elif [ x$1 = x"mcu" ]; then
-    vender=gnu_arm_embedded
-    host=arm-none-eabi
-    gcc_version=gcc-arm-none-eabi-5_4-2016q3
-    gcc_prefix=arm-none-eabi
-    cross_gcc_path=${data_disk_path}/opt/toolchains/${vender}/${gcc_version}/bin/${gcc_prefix}-
-
-    _ldflag_com="${_ldflag_com} -specs=nano.specs -specs=nosys.specs"
 else
     help_info
 fi
 
 # 3rd_lib path
-lib_3rd_path=${data_disk_path}/install/${vender}/${gcc_version}
-_cppflags_com="${_cppflags_com} -I${lib_3rd_path}/include"
-_ldflag_com="${_ldflag_com} -L${lib_3rd_path}/lib"
+prefix_path=${data_disk_path}/install/${vender}/${gcc_version}
+_cppflags_com="${_cppflags_com} -I${prefix_path}/include"
+_ldflag_com="${_ldflag_com} -L${prefix_path}/lib"
 
-# target
-target_path=`pwd`
-prefix_path=${lib_3rd_path}
+make distclean
 
-cd ${target_path} && ./autogen.sh && cd -
+cd `pwd` && ./autogen.sh && cd -
 
 if [ $# = 2 ]; then
     mkdir -p $2/${vender}
     cd $2/${vender}
 fi
 
-${target_path}/configure                                    \
+`pwd`/configure                                             \
     CC=${cross_gcc_path}gcc                                 \
     CXX=${cross_gcc_path}g++                                \
-    CPPFLAGS="${_cppflags_com} "                            \
+    CPPFLAGS="${_cppflags_com}"                             \
     CFLAGS="${_cflags_com}"                                 \
     CXXFLAGS="${_cxxflags_com}"                             \
     LDFLAGS="${_ldflag_com}"                                \
     LIBS=""                                                 \
-    PKG_CONFIG_PATH="${lib_3rd_path}/lib/pkgconfig"         \
+    PKG_CONFIG_PATH="${prefix_path}/lib/pkgconfig"          \
     --prefix=${prefix_path}                                 \
     --build=                                                \
     --host=${host}                                          \
@@ -89,5 +80,4 @@ ${target_path}/configure                                    \
 
 thread_jobs=`getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1`
 
-make -j${thread_jobs}; make install
-
+make -j${thread_jobs} && make install
